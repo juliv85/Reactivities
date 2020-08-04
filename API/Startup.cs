87325519463b2a93ac37b2426503f49340d2ae7b
Julio Vazquez <juliovazquez@Julios-MacBook-Pro.local>
2020-08-04 17:43:30 -0400

    End of the section 6

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,7 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
+using Application.Activities;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -41,7 +40,7 @@ namespace API
                     policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
                 });
             });
-            
+            services.AddMediatR(typeof(List.Handler).Assembly);
             services.AddMvc(option => option.EnableEndpointRouting = false).SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
         }
 
